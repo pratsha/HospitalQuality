d275ea9adcb9951b2f89f71e9186ca35ac1a17f3
Bhatnagar <prateekk.bhatnagar@gmail.com>
2021-08-04 14:04:40 +0530

    Added comment and Stop

--- rankhospital.R
+++ rankhospital.R
@@ -1,13 +1,29 @@
+# Would return a character vector containing the name of the hospital with the 5th lowest 
+# 30-day death rate for heart attack/heart failure/pneumonia
 rankhospital<-function(state,outcome,num="best"){
   
   caremeasures<-read.csv("outcome-of-care-measures.csv",colClasses = "character")
   caremeasures<-caremeasures[caremeasures$State==state,]
-  
+ 
   if(outcome=="heart attack")
   {
     sorted<-caremeasures[!caremeasures[,11]=="Not Available",]
-    sorted[order(as.numeric(sorted[,11])),2]
     x<-sorted[order(as.numeric(sorted[,11]),sorted[,2]),][,c(2,7,11)]
+    
+    
+    if(num=="best")
+    {
+      num<-1
+    }
+    else if(num=="worst")
+    {
+      num<-nrow(sorted)
+    }
+    
+    if(num>nrow(sorted))
+    {
+      stop("NA")
+    }
     x[num,]
     
   }
@@ -15,14 +31,42 @@ rankhospital<-function(state,outcome,num="best"){
   {
     sorted<-caremeasures[!caremeasures[,17]=="Not Available",]
     x<-sorted[order(as.numeric(sorted[,17]),sorted[,2]),][,c(2,7,17)]
+    
+    
+    if(num=="best")
+    {
+      num<-1
+    }
+    else if(num=="worst")
+    {
+      num<-nrow(sorted)
+      
+    }
+    if(num>nrow(sorted))
+    {
+      stop("NA")
+    }
     x[num,]
   
   }
   else if(outcome=="pneumonia")
   {
     sorted<-caremeasures[!caremeasures[,23]=="Not Available",]
-    sorted[order(as.numeric(sorted[,23])),][,c(2,7,23)][num,]
     x<-sorted[order(as.numeric(sorted[,23]),sorted[,2]),][,c(2,7,23)]
+    
+   
+    if(num=="best")
+    {
+      num<-1
+    }
+    else if(num=="worst")
+    {
+      num<-nrow(sorted)
+    }
+    if(num>nrow(sorted))
+    {
+      stop("NA")
+    }
     x[num,]
   }
 }
